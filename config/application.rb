2af367b64e79d34390ec4f7f8e800c63c9975e1f
Anthony Robin <contact@anthony-robin.fr>
2015-04-02 18:29:31 +0200

    Improve application configuration

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,26 @@ module Startup
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Paris'
+    config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.default_locale = :fr
+    config.i18n.locale = :fr
+    config.i18n.default_locale = :fr
+    config.i18n.available_locales = [:fr, :en]
+    config.i18n.fallbacks = true
+
+    config.autoload_paths += Dir["#{config.root}/app/controllers/**/"]
+    config.autoload_paths += Dir["#{config.root}/app/models/**/"]
+    config.autoload_paths += Dir["#{config.root}/app/decorators/**/"]
+    config.autoload_paths += Dir["#{config.root}/app/admin/**/"]
+
+    # Remove Helper, CSS, Coffee generating when scaffolding ressources
+    config.generators.helper = false
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
