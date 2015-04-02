c921c0db54b28234cf8094469a31ed7923d0133a
Anthony Robin <contact@anthony-robin.fr>
2015-04-03 00:19:24 +0200

    Add routing for contacts

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
 
   localized do
-    # You can have the root of your site routed with "root"
     root 'homes#index'
+    resources :contacts, only: [:index, :new, :create]
   end
 end
