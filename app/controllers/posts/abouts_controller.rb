e3449bf6fbb938253050292e34da78bcb8182b95
Anthony Robin <contact@anthony-robin.fr>
2015-04-07 22:46:48 +0200

    Add SEO tag for remaining controllers

--- app/controllers/posts/abouts_controller.rb
+++ app/controllers/posts/abouts_controller.rb
@@ -2,10 +2,10 @@
 # == Abouts Controller
 #
 class AboutsController < InheritedResources::Base
-
   # GET /abouts
   # GET /abouts.json
   def index
     @abouts = About.online
+    seo_tag_index category
   end
 end
