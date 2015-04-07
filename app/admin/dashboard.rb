79682c207f656f4df16a67e6851ae995b38e62e9
Anthony Robin <contact@anthony-robin.fr>
2015-04-07 11:17:05 +0200

    Setup Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,38 @@
 ActiveAdmin.register_page 'Dashboard' do
+  menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
-  menu priority: 1, label: proc{ I18n.t('active_admin.dashboard') }
+  content title: proc { I18n.t('active_admin.dashboard') } do
+    columns do
+      column do
+        panel 'Utilisateurs' do
+          table_for User.last(5) do
+            column :avatar do |user|
+              retina_image_tag(user, :avatar, :thumb)
+            end
+            column :username
+            column :email
+            column :role
+            column('Actions') do |user|
+              link_to('Voir', admin_user_path(user))
+            end
+          end
+        end
+      end # column
 
-  content title: proc{ I18n.t('active_admin.dashboard') } do
-    div class: 'blank_slate_container', id: 'dashboard_default_message' do
-      span class: 'blank_slate' do
-        span I18n.t('active_admin.dashboard_welcome.welcome')
-        small I18n.t('active_admin.dashboard_welcome.call_to_action')
-      end
-    end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel 'Recent Posts' do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel 'Info' do
-    #       para 'Welcome to ActiveAdmin.'
-    #     end
-    #   end
-    # end
+      column do
+        panel 'Settings' do
+          table_for Setting.first do
+            column :title
+            column :subtitle
+            column :show_map do |resource|
+              status_tag("#{resource.show_map}", (resource.show_map? ? :ok : :warn))
+            end
+            column('Actions') do |resource|
+              link_to(I18n.t('active_admin.edit'), edit_admin_setting_path(resource)) + ' ' + link_to('Voir', admin_setting_path(resource))
+            end
+          end
+        end
+      end # column
+    end # columns
   end # content
 end
