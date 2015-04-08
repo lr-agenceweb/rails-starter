cefbc5d2ce84bb5dddd7efc2ba2d8a8d888c6b2c
Anthony Robin <contact@anthony-robin.fr>
2015-04-08 23:07:29 +0200

    Add test to check user role

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup :initialize_settings
+
+  test 'should be true if user is super_administrator' do
+    assert @super_administrator.super_administrator?
+  end
+
+  test 'should be true if user is administrator' do
+    assert @administrator.administrator?
+  end
+
+  test 'should be true if user is subscriber' do
+    assert @subscriber.subscriber?
+  end
+
+  private
+
+  def initialize_settings
+    @super_administrator = users(:anthony)
+    @administrator = users(:bob)
+    @subscriber = users(:alice)
+  end
 end
