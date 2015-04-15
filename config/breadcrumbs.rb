d1056d46c58b9b890f729903ca31b51a234d6328
Anthony Robin <contact@anthony-robin.fr>
2015-04-15 14:23:16 +0200

    Remove unusued code

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -14,19 +14,3 @@ end
 crumb :contact do
   link Category.includes(:translations).title_by_category('Contact'), new_contact_path
 end
-
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
