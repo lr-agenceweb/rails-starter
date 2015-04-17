75cdd83c126cd665c35fbbb8a9bf129eebab9ded
Anthony Robin <contact@anthony-robin.fr>
2015-04-18 01:36:02 +0200

    Refactor gon params into a helper method

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -59,18 +59,7 @@ ActiveAdmin.register_page 'Dashboard' do
 
     def set_setting
       @setting = Setting.first
-      gon.push(
-        mapbox_username: Figaro.env.mapbox_username,
-        mapbox_key: Figaro.env.mapbox_map_key,
-        mapbox_access_token: Figaro.env.mapbox_access_token,
-        name: @setting.name,
-        subtitle: @setting.subtitle,
-        address: @setting.address,
-        city: @setting.city,
-        postcode: @setting.postcode,
-        latitude: @setting.latitude,
-        longitude: @setting.longitude
-      )
+      gon_params
     end
   end
 end
