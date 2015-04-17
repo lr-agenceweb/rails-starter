1ef84ca1f40c845c3730a8be7e1a778fc8c421f9
Anthony Robin <contact@anthony-robin.fr>
2015-04-18 00:53:40 +0200

    Try to make travis work for postgresql

--- db/migrate/20150406222613_add_attachment_avatar_to_users.rb
+++ db/migrate/20150406222613_add_attachment_avatar_to_users.rb
@@ -2,7 +2,7 @@ class AddAttachmentAvatarToUsers < ActiveRecord::Migration
   def self.up
     change_table :users do |t|
       add_attachment :users, :avatar
-      add_column :users, :retina_dimensions, :text, after: :email
+      add_column :users, :retina_dimensions, :text, after: :avatar_file_name
     end
   end
 
