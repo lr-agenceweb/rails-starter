905498e9662ff3bf23c8aba3f16719fa0346130a
Anthony Robin <contact@anthony-robin.fr>
2015-06-08 21:50:45 +0200

    Fix bug with boolean and postgre

--- db/migrate/20150608190551_add_boolean_to_settings.rb
+++ db/migrate/20150608190551_add_boolean_to_settings.rb
@@ -1,6 +1,6 @@
 class AddBooleanToSettings < ActiveRecord::Migration
   def change
-    add_column :settings, :show_breadcrumb, :boolean, default: 0, after: :show_map
-    add_column :settings, :show_social, :boolean, default: 1, after: :show_breadcrumb
+    add_column :settings, :show_breadcrumb, :boolean, default: false, after: :show_map
+    add_column :settings, :show_social, :boolean, default: true, after: :show_breadcrumb
   end
 end
