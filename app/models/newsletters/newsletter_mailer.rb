0f4a015e64f6d9946d627a148f1b9147e2b4092c
Anthony Robin <contact@anthony-robin.fr>
2015-06-09 23:09:51 +0200

    Setup newsletter mailer model, views, and preview

---
+++ app/models/newsletters/newsletter_mailer.rb
@@ -0,0 +1,31 @@
+#
+# == Newsletter Mailer
+#
+class NewsletterMailer < ActionMailer::Base
+  add_template_helper(ApplicationHelper)
+  default from: Setting.first.email
+
+  def welcome_user(newsletter_user)
+    @newsletter_user = newsletter_user
+    @newsletter_user.name = @newsletter_user.email.split('@').first
+    @title = t('newsletter.welcome')
+    @host = Figaro.env.application_host
+    @is_welcome_user = true
+
+    mail(to: @newsletter_user.email, subject: @title) do |format|
+      format.html { render layout: 'newsletter' }
+      format.text
+    end
+  end
+
+  # def send_newsletter(newsletter_user, newsletter)
+  #   @newsletter_user = newsletter_user
+  #   @newsletter = newsletter
+  #   @title = @newsletter.title
+  #   @host = Figaro.env.application_host
+  #   mail(to: @newsletter_user.email, subject: @newsletter.title) do |format|
+  #     format.html { render layout: 'newsletter' }
+  #     format.text
+  #   end
+  # end
+end
