77ca3549a6ad8d8b501add4be2b4e8f726771166
Anthony Robin <contact@anthony-robin.fr>
2015-06-09 23:12:39 +0200

    Add routes for newsletters and newsletter_users

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     get '(page/:page)', action: :index, on: :collection, as: ''
   end
 
+  # Newsletters
+  resources :newsletter_users, only: [:create]
+  get '/newsletter/unsubscribe/:newsletter_user_id/:token', to: 'newsletter_users#unsubscribe', as: :unsubscribe
+
   localized do
     root 'homes#index'
     resources :abouts, only: [:index, :show], concerns: :paginatable do
@@ -15,5 +19,8 @@ Rails.application.routes.draw do
     resources :contact_forms, controller: 'contacts', only: [:index, :new, :create]
 
     get 'feed', to: 'posts#feed', as: :posts_rss
+
+    get '/newsletter/welcome_user/:newsletter_user_id/:token', to: 'newsletters#welcome_user', as: :welcome_user
+    get '/newsletter/:id/:newsletter_user_id/:token', to: 'newsletters#show', as: :show_newsletter
   end
 end
