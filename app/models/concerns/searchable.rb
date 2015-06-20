d13aa9f3424b5aae11018f6d5c6c59de42482523
Anthony Robin <contact@anthony-robin.fr>
2015-06-20 18:03:59 +0200

    Add condition to fetch only online posts

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -5,16 +5,10 @@ module Searchable
   extend ActiveSupport::Concern
 
   included do
-    # before_create :generate_token
-
     def self.search(search, locale)
       if search
         wildcard_search = "%#{search}%"
-        with_translations(locale).where("#{self}_translations.title LIKE ? OR #{self}_translations.content LIKE ?", wildcard_search, wildcard_search)
-        # .page(page)
-        # .per_page(5)
-      else
-        with_translations(locale)
+        with_translations(locale).online.where("#{self}_translations.title LIKE ? OR #{self}_translations.content LIKE ?", wildcard_search, wildcard_search)
       end
     end
   end
