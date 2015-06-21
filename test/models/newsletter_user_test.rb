b1b33d73695096bf1f584b2b01353d5af3292028
Anthony Robin <contact@anthony-robin.fr>
2015-06-21 16:00:40 +0200

    Add test for newsletter user model method

--- test/models/newsletter_user_test.rb
+++ test/models/newsletter_user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
+#
+# == NewsletterUser model test
+#
 class NewsletterUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup :initialize_test
+
+  test 'should have testers' do
+    NewsletterUser.create(email: @email, lang: @lang, role: 'tester')
+    assert NewsletterUser.testers?
+  end
+
+  test 'should not have testers' do
+    NewsletterUser.create(email: @email, lang: @lang, role: 'subscriber')
+    assert_not NewsletterUser.testers?
+  end
+
+  private
+
+  def initialize_test
+    @email = 'aaa@bbb.cc'
+    @lang = 'fr'
+  end
 end
