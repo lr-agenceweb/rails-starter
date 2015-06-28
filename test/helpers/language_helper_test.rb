86a8117a290b343c8465656384c9c952019e9393
Anthony Robin <contact@anthony-robin.fr>
2015-06-28 19:25:14 +0200

    Add more tests for language helper

--- test/helpers/language_helper_test.rb
+++ test/helpers/language_helper_test.rb
@@ -5,6 +5,9 @@ require 'test_helper'
 #
 class LanguageHelperTest < ActionView::TestCase
   include LanguageHelper
+  include FontAwesome::Rails::IconHelper
+
+  setup :initialize_test
 
   test 'should return true if current locale is right' do
     params[:locale] = :fr
@@ -14,5 +17,28 @@ class LanguageHelperTest < ActionView::TestCase
   test 'should return false if current locale is wrong' do
     params[:locale] = :fr
     assert_not current_locale?(:en)
+  end
+
+  test 'should return correct class if tested locale is true' do
+    params[:locale] = :fr
+    assert_equal 'l-nav-item-active', active_language(:fr)
+    assert_nil active_language(:en)
+  end
+
+  test 'should correctly format in html link language' do
+    params[:locale] = :fr
+    assert_equal '<a class="l-nav-item-link" href="#">Français <i class="fa fa-check"></i></a>', current_link_language('check', I18n.t("active_admin.globalize.language.#{params[:locale]}"))
+  end
+
+  test 'should return correct slug article by locale' do
+    params[:locale] = :fr
+    assert_equal 'developpement-hebergement', slug_for_locale(posts(:about), :fr)
+    assert_equal 'site-hosting', slug_for_locale(posts(:about), :en)
+  end
+
+  private
+
+  def initialize_test
+    @locales = I18n.available_locales
   end
 end
