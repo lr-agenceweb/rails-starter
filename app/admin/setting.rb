38d020207a03a91921491c2e0695ea42e62173b6
Anthony Robin <contact@anthony-robin.fr>
2015-07-01 17:51:53 +0200

    Update AA setting#form style

--- app/admin/setting.rb
+++ app/admin/setting.rb
@@ -84,47 +84,62 @@ ActiveAdmin.register Setting do
   form do |f|
     f.semantic_errors *f.object.errors.keys
 
-    f.inputs 'Paramètres du site' do
-      f.translated_inputs 'Translated fields', switch_locale: false do |t|
-        t.input :title, hint: 'Titre du site'
-        t.input :subtitle, hint: 'Sous-titre du site'
+    columns do
+      column do
+        f.inputs 'Paramètres du site' do
+          f.input :maintenance, hint: 'Mettre le site en maintenance a pour effet de rendre le contenu inaccessible sur internet'
+          f.translated_inputs 'Translated fields', switch_locale: false do |t|
+            t.input :title, hint: 'Titre du site'
+            t.input :subtitle, hint: 'Sous-titre du site'
+          end
+        end
       end
-      f.input :maintenance, hint: 'Mettre le site en maintenance a pour effet de rendre le contenu inaccessible sur internet'
+
+      column do
+        f.inputs 'Paramètres de l\'administrateur' do
+          f.input :name, hint: 'Nom du propriétaire du site'
+          f.input :email, hint: 'Email d\'où seront reçus les messages de contact'
+          f.input :phone, as: :phone, hint: 'Numéro de téléphone à afficher sur le site pour vous joindre'
+        end
+      end
     end
 
-    f.inputs 'Paramètres des modules' do
-      f.input :show_breadcrumb, hint: 'Afficher ou non le fil d\'ariane sur le site'
-      f.input :show_social, hint: 'Afficher ou non les icônes de partage social sur le site'
-      f.input :should_validate, hint: 'Si coché, les messages postés dans le livre d\'or et les commentaires ne seront pas visibles tant que vous ne les aurez pas validé manuellement'
-      f.input :show_map, hint: 'Afficher ou non la carte sur la page contact'
-    end
+    columns do
+      column do
+        f.inputs 'Paramètres des modules' do
+          f.input :show_breadcrumb, hint: 'Afficher ou non le fil d\'ariane sur le site'
+          f.input :show_social, hint: 'Afficher ou non les icônes de partage social sur le site'
+          f.input :should_validate, hint: 'Si coché, les messages postés dans le livre d\'or et les commentaires ne seront pas visibles tant que vous ne les aurez pas validé manuellement'
+          f.input :show_map, hint: 'Afficher ou non la carte sur la page contact'
+        end
+      end
 
-    f.inputs 'Paramètres de l\'administrateur' do
-      f.input :name, hint: 'Nom du propriétaire du site'
-      f.input :email, hint: 'Email d\'où seront reçus les messages de contact'
-      f.input :phone, as: :phone, hint: 'Numéro de téléphone à afficher sur le site pour vous joindre'
-    end
+      column do
+        f.inputs 'Paramètre de la carte', class: 'map-settings' do
+          f.input :geocode_address,
+                  hint: 'Ce champs est utilisé pour récupérer les coordonnées latitude / longitude de la position et centrer la carte (n\'est pas affichée)',
+                  input_html: { id: 'gmaps-input-address' }
 
-    f.inputs 'Paramètre de la carte', class: 'map-settings' do
-      f.input :geocode_address,
-              hint: 'Ce champs est utilisé pour récupérer les coordonnées latitude / longitude de la position et centrer la carte (n\'est pas affichée)',
-              input_html: { id: 'gmaps-input-address' }
-
-      f.input :address,
-              hint: 'Adresse affichée sur le site'
-      f.input :city
-      f.input :postcode
-      f.input :latitude,
-              label: false,
-              input_html: { id: 'gmaps-output-latitude', class: 'hide' }
-      f.input :longitude,
-              label: false,
-              input_html: { id: 'gmaps-output-longitude', class: 'hide' }
+          f.input :address,
+                  hint: 'Adresse affichée sur le site'
+          f.input :city
+          f.input :postcode
+          f.input :latitude,
+                  label: false,
+                  input_html: { id: 'gmaps-output-latitude', class: 'hide' }
+          f.input :longitude,
+                  label: false,
+                  input_html: { id: 'gmaps-output-longitude', class: 'hide' }
+        end
+      end
     end
 
     f.actions
   end
 
+  #
+  # == Controller
+  #
   controller do
     before_action :set_setting, only: [:show]
 
