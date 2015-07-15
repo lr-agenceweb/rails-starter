aa7c005e892d5e3bc3ce5a61f8ae5d676f9d6655
Anthony Robin <contact@anthony-robin.fr>
2015-07-15 17:41:29 +0200

    Use single quote instead of double for simple string

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,5 +3,5 @@ set :output, error: "#{path}/log/error.log", standard: "#{path}/log/cron.log"
 every 1.day, at: '4:00 am' do
   rake '-s sitemap:refresh'
   rake 'db:backup MAX=10'
-  command "backup perform -t rails_starter"
+  command 'backup perform -t rails_starter'
 end
