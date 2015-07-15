f9b87a0712b63ef36c37831f40163245ecd77713
Anthony Robin <contact@anthony-robin.fr>
2015-07-15 16:46:56 +0200

    Update schedule.rb removing production constraint

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,5 +5,5 @@ set :output, error: "#{path}/log/error.log", standard: "#{path}/log/cron.log"
 every 1.day, at: '4:00 am' do
   rake '-s sitemap:refresh'
   rake 'db:backup MAX=10'
-  command "backup perform -t #{Figaro.env.application_name}", environment: 'production'
+  command "backup perform -t #{Figaro.env.application_name}"
 end
