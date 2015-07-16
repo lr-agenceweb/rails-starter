b2d0315963c785c5267c666f57a75168327cce02
Anthony Robin <contact@anthony-robin.fr>
2015-07-16 02:03:33 +0200

    Setup autocomplete on search form

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,14 +2,16 @@
 # == SearchesController
 #
 class SearchesController < ApplicationController
+  before_action :set_gon_autocomplete
+
   def index
-    if params[:query].nil? || params[:query].blank?
+    if params[:term].nil? || params[:term].blank?
       @searches = []
     else
-      @searches = Post.search(params[:query], params[:locale])
+      @searches = Post.search(params[:term], params[:locale])
 
       if @blog_module.enabled?
-        @searches += Blog.search(params[:query], params[:locale])
+        @searches += Blog.search(params[:term], params[:locale])
       end
 
       @searches = Kaminari.paginate_array(@searches).page params[:page]
@@ -18,6 +20,13 @@ class SearchesController < ApplicationController
     respond_to do |format|
       format.html
       format.js
+      format.json
     end
+  end
+
+  private
+
+  def set_gon_autocomplete
+    gon.push(search_path: searches_path(format: :json))
   end
 end
