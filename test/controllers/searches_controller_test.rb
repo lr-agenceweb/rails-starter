0a55a8eebc85b26d9944fe226227364c9c2ad2dc
Anthony Robin <contact@anthony-robin.fr>
2015-07-16 02:41:45 +0200

    Fix test after renaming query to term

--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -34,74 +34,74 @@ class SearchesControllerTest < ActionController::TestCase
     assert_routing '/en/search', controller: 'searches', action: 'index', locale: 'en' if @locales.include?(:en)
   end
 
-  test 'should return empty object if query is not set' do
+  test 'should return empty object if term is not set' do
     @locales.each do |locale|
       get :index, locale: locale.to_s
       assert_empty assigns(:searches)
     end
   end
 
-  test 'should return empty object if query is empty' do
+  test 'should return empty object if term is empty' do
     @locales.each do |locale|
-      get :index, locale: locale.to_s, query: ''
+      get :index, locale: locale.to_s, term: ''
       assert_empty assigns(:searches)
     end
   end
 
-  test 'should return empty object if query not in post articles' do
+  test 'should return empty object if term not in post articles' do
     @locales.each do |locale|
-      get :index, locale: locale.to_s, query: 'Unitary tests'
+      get :index, locale: locale.to_s, term: 'Unitary tests'
       assert_empty assigns(:searches)
     end
   end
 
-  test 'should return full object if query in post articles' do
-    query = 'Ruby'
+  test 'should return full object if term in post articles' do
+    term = 'Ruby'
     @locales.each do |locale|
-      get :index, locale: locale.to_s, query: query
+      get :index, locale: locale.to_s, term: term
       searches = assigns(:searches)
       assert_not_empty searches
-      assert_match(/#{query}/, searches.first.title)
-      assert_match(/#{query}/, searches.first.content)
+      assert_match(/#{term}/, searches.first.title)
+      assert_match(/#{term}/, searches.first.content)
     end
   end
 
   test 'should not take care of offline post in search results' do
-    query = 'Ruby'
+    term = 'Ruby'
     @locales.each do |locale|
-      get :index, locale: locale.to_s, query: query
+      get :index, locale: locale.to_s, term: term
       searches = assigns(:searches)
       assert_equal searches.count, 1
     end
   end
 
   test 'should return different result by locale' do
-    query = 'Hébergement'
+    term = 'Hébergement'
     I18n.with_locale(:fr) do
-      get :index, locale: 'fr', query: query
+      get :index, locale: 'fr', term: term
       searches = assigns(:searches)
       assert_not_empty searches
-      assert_match(/#{query}/, searches.first.title)
+      assert_match(/#{term}/, searches.first.title)
     end
 
     if @locales.include?(:en)
       I18n.with_locale(:en) do
-        get :index, locale: 'en', query: query
+        get :index, locale: 'en', term: term
         assert_empty assigns(:searches)
       end
     end
   end
 
   # == Ajax
-  test 'should execute query in ajax' do
-    query = 'Ruby'
-    xhr :get, :index, format: :js, query: query, locale: 'fr'
+  test 'should execute term in ajax' do
+    term = 'Ruby'
+    xhr :get, :index, format: :js, term: term, locale: 'fr'
     assert_response :success
   end
 
   test 'should not be empty in ajax' do
-    query = 'Hébergement'
-    xhr :get, :index, format: :js, query: query, locale: 'fr'
+    term = 'Hébergement'
+    xhr :get, :index, format: :js, term: term, locale: 'fr'
     assert_not_empty assigns(:searches)
     assert assigns(:searches).count, 1
   end
