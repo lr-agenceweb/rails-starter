15fcdc021e098e4f3c75a1312fca26767bd8755e
Anthony Robin <contact@anthony-robin.fr>
2015-07-17 17:08:20 +0200

    Create model to handle custom messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717003134) do
+ActiveRecord::Schema.define(version: 20150717150606) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -317,6 +317,28 @@ ActiveRecord::Schema.define(version: 20150717003134) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "string_box_translations", force: :cascade do |t|
+    t.integer  "string_box_id", limit: 4,     null: false
+    t.string   "locale",        limit: 255,   null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "title",         limit: 255
+    t.text     "content",       limit: 65535
+  end
+
+  add_index "string_box_translations", ["locale"], name: "index_string_box_translations_on_locale", using: :btree
+  add_index "string_box_translations", ["string_box_id"], name: "index_string_box_translations_on_string_box_id", using: :btree
+
+  create_table "string_boxes", force: :cascade do |t|
+    t.string   "key",        limit: 255
+    t.string   "title",      limit: 255
+    t.text     "content",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "string_boxes", ["key"], name: "index_string_boxes_on_key", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username",               limit: 255
---
+++ test/models/string_box_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StringBoxTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
