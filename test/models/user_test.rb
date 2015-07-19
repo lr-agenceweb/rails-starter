3a0b794287e2c9cc3d137917092e9f220e450ab3
Anthony Robin <contact@anthony-robin.fr>
2015-07-20 01:11:41 +0200

    Add test for user avatar upload

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,6 +4,8 @@ require 'test_helper'
 # == User model test
 #
 class UserTest < ActiveSupport::TestCase
+  include ActionDispatch::TestProcess
+
   setup :initialize_test
 
   #
@@ -31,6 +33,40 @@ class UserTest < ActiveSupport::TestCase
 
   test 'should be false if user avatar is nil' do
     assert_not @super_administrator.avatar?
+  end
+
+  test 'should not upload avatar if mime type is not allowed' do
+    assert_nil @subscriber.avatar.path(:original)
+    assert_nil @subscriber.avatar.path(:large)
+    assert_nil @subscriber.avatar.path(:medium)
+    assert_nil @subscriber.avatar.path(:small)
+    assert_nil @subscriber.avatar.path(:thumb)
+
+    attachment = fixture_file_upload 'images/fake.txt', 'text/plain'
+    @subscriber.update_attributes(avatar: attachment)
+
+    assert_not_processed 'fake.txt', :original, @subscriber.avatar
+    assert_not_processed 'fake.txt', :large, @subscriber.avatar
+    assert_not_processed 'fake.txt', :medium, @subscriber.avatar
+    assert_not_processed 'fake.txt', :small, @subscriber.avatar
+    assert_not_processed 'fake.txt', :thumb, @subscriber.avatar
+  end
+
+  test 'should upload avatar if mime type is allowed' do
+    assert_nil @subscriber.avatar.path(:original)
+    assert_nil @subscriber.avatar.path(:large)
+    assert_nil @subscriber.avatar.path(:medium)
+    assert_nil @subscriber.avatar.path(:small)
+    assert_nil @subscriber.avatar.path(:thumb)
+
+    attachment = fixture_file_upload 'images/bart.png', 'image/png'
+    @subscriber.update_attributes!(avatar: attachment)
+
+    assert_processed 'bart.png', :original, @subscriber.avatar
+    assert_processed 'bart.png', :large, @subscriber.avatar
+    assert_processed 'bart.png', :medium, @subscriber.avatar
+    assert_processed 'bart.png', :small, @subscriber.avatar
+    assert_processed 'bart.png', :thumb, @subscriber.avatar
   end
 
   private
