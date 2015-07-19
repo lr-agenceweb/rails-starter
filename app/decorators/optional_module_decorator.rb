73f0551cfaf6cbbe262139a76f6724a2db5697e7
Anthony Robin <contact@anthony-robin.fr>
2015-07-19 14:32:08 +0200

    Add I18n translation for OptionalModules in dashboard

--- app/decorators/optional_module_decorator.rb
+++ app/decorators/optional_module_decorator.rb
@@ -10,4 +10,8 @@ class OptionalModuleDecorator < ApplicationDecorator
     color = model.enabled? ? 'green' : 'red'
     status_tag_deco(I18n.t("enabled.#{model.enabled}"), color)
   end
+
+  def name
+    t("optional_module.name.#{model.name.underscore.downcase}")
+  end
 end
