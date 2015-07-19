a64d7d814fd7c1c12593a410b2d2aa4f8f17be17
Anthony Robin <contact@anthony-robin.fr>
2015-07-19 23:10:43 +0200

    Add Attachable concern to handle paperclip options depending on environment

---
+++ app/models/concerns/attachable.rb
@@ -0,0 +1,30 @@
+#
+# == Attachable module
+#
+module Attachable
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def has_attachment(name, options = {})
+      model_name = table_name
+      attachment_path  = "#{model_name}/:id/:style-:filename"
+
+      # conditionals options
+      if Rails.env.staging? || Rails.env.production?
+        options[:storage] ||= :dropbox
+        options[:dropbox_credentials] ||= File.join(Rails.root, 'config', 'dropbox.yml')
+        options[:path] ||= attachment_path
+      else
+        options[:path] ||= ":rails_root/public/system/#{Rails.env}/#{attachment_path}"
+        options[:url] ||= "/system/#{Rails.env}/#{attachment_path}"
+      end
+
+      # default options
+      options[:retina]      = { quality: 70 }
+      options[:default_url] = '/default/:style-missing.png'
+
+      # paperclip
+      has_attached_file name, options
+    end
+  end
+end
