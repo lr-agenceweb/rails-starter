14e2edd11cce50454230fb7d3b2a5aafe296addf
Anthony Robin <contact@anthony-robin.fr>
2015-07-20 18:25:16 +0200

    Add status_tag for newsletter_users role

--- app/decorators/optional_modules/newsletter_user_decorator.rb
+++ app/decorators/optional_modules/newsletter_user_decorator.rb
@@ -4,4 +4,21 @@
 class NewsletterUserDecorator < ApplicationDecorator
   include Draper::LazyHelpers
   delegate_all
+
+  def role
+    color = 'orange' if tester?
+    color = 'green' if subscriber?
+
+    arbre do
+      status_tag I18n.t("role.#{model.role}"), color
+    end
+  end
+
+  def tester?
+    model.role == 'tester'
+  end
+
+  def subscriber?
+    model.role == 'subscriber'
+  end
 end
