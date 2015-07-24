2185cbaee054a8d0a80b4032effc734c51d3fd0a
Anthony Robin <contact@anthony-robin.fr>
2015-07-24 18:52:19 +0200

    Add tests for events_controller

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
+#
+# == Event model test
+#
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should return correct count for events' do
+    assert_equal 3, Event.count
+  end
+
+  test 'should return correct count for online events' do
+    assert_equal 2, Event.online.count
+  end
 end
