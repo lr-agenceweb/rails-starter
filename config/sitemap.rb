5d9514f6976735e5564064ac4f53af96ed7b03e2
Anthony Robin <contact@anthony-robin.fr>
2015-07-24 23:35:13 +0200

    Add OptionalModule links to sitemap if enabled

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,3 +1,5 @@
+SitemapGenerator::Interpreter.send :include, SitemapHelper
+
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = Figaro.env.application_host
 SitemapGenerator::Sitemap.verbose = true
@@ -6,16 +8,20 @@ SitemapGenerator::Sitemap.sitemaps_path = 'shared/'
 
 SitemapGenerator::Sitemap.create do
   group(sitemaps_path: 'sitemap/fr/', filename: :french) do
-    add root_path
-    add abouts_path, priority: 0.7, changefreq: 'monthly'
-    add new_contact_path, priority: 0.7, changefreq: 'monthly'
+    classic_links
+    blog_module if OptionalModule.by_name('Blog').enabled?
+    event_module if OptionalModule.by_name('Event').enabled?
+    guest_book_module if OptionalModule.by_name('GuestBook').enabled?
   end
 
-  group(sitemaps_path: 'sitemap/en/', filename: :english) do
-    I18n.with_locale(:en) do
-      add root_path
-      add abouts_path, priority: 0.7, changefreq: 'monthly'
-      add new_contact_path, priority: 0.7, changefreq: 'monthly'
+  if I18n.available_locales.include?(:en)
+    group(sitemaps_path: 'sitemap/en/', filename: :english) do
+      I18n.with_locale(:en) do
+        classic_links
+        blog_module if OptionalModule.by_name('Blog').enabled?
+        event_module if OptionalModule.by_name('Event').enabled?
+        guest_book_module if OptionalModule.by_name('GuestBook').enabled?
+      end
     end
   end
 end
