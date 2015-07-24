3662ef7f8f8bcde233a4b21373cf00a32000ae46
Anthony Robin <contact@anthony-robin.fr>
2015-07-24 18:45:54 +0200

    Improve test for Blog model

--- test/models/blog_test.rb
+++ test/models/blog_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
+#
+# == Blog model test
+#
 class BlogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should return correct count for blogs posts' do
+    assert_equal 2, Blog.count
+  end
+
+  test 'should fetch only online blog posts' do
+    assert_equal 1, Blog.online.count
+  end
 end
