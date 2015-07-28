92d1d433ed28626c50cf5d908f1a995b93a28cb5
Anthony Robin <contact@anthony-robin.fr>
2015-07-28 23:34:53 +0200

    Add test to ensure author about article is correct

--- test/models/about_test.rb
+++ test/models/about_test.rb
@@ -4,7 +4,15 @@ require 'test_helper'
 # == About model test
 #
 class AboutTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup :initialize_test
+
+  test 'should have correct user for article' do
+    assert_equal 'anthony', @about.user_username
+  end
+
+  private
+
+  def initialize_test
+    @about = posts(:about)
+  end
 end
