8a250743983829e6d79429c19341568f06e83e5b
Anthony Robin <contact@anthony-robin.fr>
2015-07-29 17:07:08 +0200

    Add test to ensure rules are followed before to upload ikon

--- test/models/social_test.rb
+++ test/models/social_test.rb
@@ -4,6 +4,10 @@ require 'test_helper'
 # == Social Model test
 #
 class SocialTest < ActiveSupport::TestCase
+  include ActionDispatch::TestProcess
+
+  setup :initialize_test
+
   test 'should count social network with share kind' do
     assert_equal 1, Social.share.count
   end
@@ -22,5 +26,40 @@ class SocialTest < ActiveSupport::TestCase
   test 'should return list of allowed kind for social newtwork' do
     assert_includes Social.allowed_kind_social_network, 'follow'
     assert_includes Social.allowed_kind_social_network, 'share'
+  end
+
+  #
+  # == Ikon
+  #
+  test 'should not upload ikon if mime type is not allowed' do
+    [:original, :large, :medium, :small, :thumb].each do |size|
+      assert_nil @social.ikon.path(size)
+    end
+
+    attachment = fixture_file_upload 'images/fake.txt', 'text/plain'
+    @social.update_attributes(ikon: attachment)
+
+    [:original, :large, :medium, :small, :thumb].each do |size|
+      assert_not_processed 'fake.txt', size, @social.ikon
+    end
+  end
+
+  test 'should upload ikon if mime type is allowed' do
+    [:original, :large, :medium, :small, :thumb].each do |size|
+      assert_nil @social.ikon.path(size)
+    end
+
+    attachment = fixture_file_upload 'images/social.png', 'image/png'
+    @social.update_attributes!(ikon: attachment)
+
+    [:original, :large, :medium, :small, :thumb].each do |size|
+      assert_processed 'social.png', size, @social.ikon
+    end
+  end
+
+  private
+
+  def initialize_test
+    @social = socials(:facebook_follow)
   end
 end
