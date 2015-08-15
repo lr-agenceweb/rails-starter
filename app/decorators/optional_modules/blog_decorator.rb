328195b94240fc0c2ada0d10d2042bf2fb165792
Anthony Robin <contact@anthony-robin.fr>
2015-08-15 18:52:50 +0200

    Refactor schema.org metadata for Blog articles

--- app/decorators/optional_modules/blog_decorator.rb
+++ app/decorators/optional_modules/blog_decorator.rb
@@ -2,5 +2,20 @@
 # == BlogDecorator
 #
 class BlogDecorator < PostDecorator
+  include Draper::LazyHelpers
   delegate_all
+
+  #
+  # Microdatas
+  #
+  def microdata_meta
+    content_tag(:div, '', itemscope: '', itemtype: 'http://schema.org/BlogPosting') do
+      concat(tag(:meta, itemprop: 'headline', content: model.title))
+      concat(tag(:meta, itemprop: 'articleBody', content: model.content)) if content?
+      concat(tag(:meta, itemprop: 'url', content: show_page_link(true)))
+      concat(tag(:meta, itemprop: 'creator', content: model.user_username))
+      concat(tag(:meta, itemprop: 'datePublished', content: model.created_at.to_datetime))
+      concat(tag(:meta, itemprop: 'image', content: attachment_url(model.first_pictures_image, :medium))) if model.pictures?
+    end
+  end
 end
