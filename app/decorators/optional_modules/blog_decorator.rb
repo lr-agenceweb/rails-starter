f921d8d496bdf2eb94838e576cbbe828ed0267f9
Anthony Robin <contact@anthony-robin.fr>
2015-08-15 20:32:05 +0200

    Improve AA title for Blog and Post show pages

--- app/decorators/optional_modules/blog_decorator.rb
+++ app/decorators/optional_modules/blog_decorator.rb
@@ -5,6 +5,10 @@ class BlogDecorator < PostDecorator
   include Draper::LazyHelpers
   delegate_all
 
+  def title_aa_show
+    I18n.t('post.title_aa_show', page: I18n.t('activerecord.models.blog.other') )
+  end
+
   #
   # Microdatas
   #
--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -73,6 +73,10 @@ class PostDecorator < ApplicationDecorator
     Category.title_by_category(type)
   end
 
+  def title_aa_show
+    I18n.t('post.title_aa_show', page: resource.type_title )
+  end
+
   #
   # Microdatas
   #
