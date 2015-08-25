5f7a4ef6d804f5025197f85e566e71b2d78851c9
Anthony Robin <contact@anthony-robin.fr>
2015-08-25 20:41:06 +0200

    Improve page title for Background#show and edit views

--- app/decorators/assets/background_decorator.rb
+++ app/decorators/assets/background_decorator.rb
@@ -21,11 +21,11 @@ class BackgroundDecorator < ApplicationDecorator
   # ActiveAdmin
   #
   def title_aa_show
-    "#{I18n.t('activerecord.models.background.one')} lié à la page #{category_name}"
+    "#{I18n.t('activerecord.models.background.one')} lié à la page \"#{category_name}\""
   end
 
   def title_aa_edit
-    "#{t('active_admin.edit')} #{I18n.t('activerecord.models.background.one')} page #{category_name}"
+    "#{t('active_admin.edit')} #{I18n.t('activerecord.models.background.one')} page \"#{category_name}\""
   end
 
   private
