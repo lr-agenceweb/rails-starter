c6d8305553f5142d25ee6352db5fe238e29fc407
Anthony Robin <contact@anthony-robin.fr>
2015-09-15 01:11:16 +0200

    Add test for Menu model

--- test/models/menu_test.rb
+++ test/models/menu_test.rb
@@ -4,16 +4,16 @@ require 'test_helper'
 # == Menu model test
 #
 class MenuTest < ActiveSupport::TestCase
-  # # TODO: Fix these tests
-  # test 'should return only visible header element' do
-  #   assert_equal 5, Category.visible_header.count
-  # end
+  # TODO: Fix these tests
+  test 'should return only visible header element' do
+    assert_equal 6, Menu.visible_header.length
+  end
+
+  test 'should return only visible footer element' do
+    assert_equal 0, Menu.visible_footer.length
+  end
 
   # test 'should return only visible header element with allowed modules' do
   #   assert_equal 5, Category.with_allowed_module.visible_header.count
-  # end
-
-  # test 'should return only visible footer element' do
-  #   assert_equal 1, Category.visible_footer.count
   # end
 end
