3c1f4fbbea25ed6119d3a0fba207c9f5cdccbbaa
Anthony Robin <contact@anthony-robin.fr>
2015-10-01 17:44:16 +0200

    Run migrations

--- app/models/optional_modules/newsletters/newsletter_setting.rb
+++ app/models/optional_modules/newsletters/newsletter_setting.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: newsletter_settings
+#
+#  id                 :integer          not null, primary key
+#  send_welcome_email :boolean          default(TRUE)
+#  title_subscriber   :string(255)
+#  content_subscriber :text(65535)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 #
 # == NewsletterSetting Model
 #
