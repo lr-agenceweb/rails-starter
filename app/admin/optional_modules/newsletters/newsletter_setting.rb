c5f433b5899108aed8f68e8a763b6f525677d87f
Anthony Robin <contact@anthony-robin.fr>
2015-10-01 17:56:19 +0200

    Setup AA NewsletterSetting resource views

---
+++ app/admin/optional_modules/newsletters/newsletter_setting.rb
@@ -0,0 +1,53 @@
+ActiveAdmin.register NewsletterSetting do
+  menu parent: I18n.t('admin_menu.config')
+
+  permit_params :id,
+                :send_welcome_email,
+                :title_subscriber,
+                :content_subscriber
+
+  decorate_with NewsletterSettingDecorator
+  config.clear_sidebar_sections!
+
+  index do
+    selectable_column
+    column :send_welcome_email
+    column :title_subscriber
+    column :content_subscriber
+
+    actions
+  end
+
+  form do |f|
+    f.semantic_errors(*f.object.errors.keys)
+
+    columns do
+      column do
+        f.inputs t('genereal') do
+          f.input :send_welcome_email,
+                  input_html: { disabled: :disabled }
+        end
+      end
+
+      column do
+        f.inputs t('activerecord.models.newsletter_setting.one') do
+          f.translated_inputs 'Translated fields', switch_locale: false do |t|
+            t.input :title_subscriber, hint: I18n.t('form.hint.title_newsletter')
+            t.input :content_subscriber,
+                    hint: I18n.t('form.hint.content_newsletter'),
+                    input_html: { class: 'froala' }
+          end
+        end
+      end
+    end
+
+    f.actions
+  end
+
+  #
+  # == Controller
+  #
+  controller do
+    include Skippable
+  end
+end
