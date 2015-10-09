6e63d7fb32aa735a450e4631d83a24337e48077e
Anthony Robin <contact@anthony-robin.fr>
2015-10-09 14:02:03 +0200

    Add status_tag for VideoSetting boolean in AA

--- app/decorators/optional_modules/videos/video_setting_decorator.rb
+++ app/decorators/optional_modules/videos/video_setting_decorator.rb
@@ -4,4 +4,14 @@
 class VideoSettingDecorator < ApplicationDecorator
   include Draper::LazyHelpers
   delegate_all
+
+  def video_platform_d
+    color = model.video_platform? ? 'green' : 'red'
+    status_tag_deco I18n.t("enabled.#{model.video_platform}"), color
+  end
+
+  def video_upload_d
+    color = model.video_upload? ? 'green' : 'red'
+    status_tag_deco I18n.t("enabled.#{model.video_upload}"), color
+  end
 end
