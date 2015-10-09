90b8b887607d076406f9778eff1054aecc34ee46
Anthony Robin <contact@anthony-robin.fr>
2015-10-09 02:36:33 +0200

    Create VideoSetting model

---
+++ app/models/optional_modules/assets/videos/video_setting.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: video_settings
+#
+#  id             :integer          not null, primary key
+#  video_platform :boolean          default(TRUE)
+#  video_upload   :boolean          default(TRUE)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
+#
+# == VideoSetting Model
+#
+class VideoSetting < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008155313) do
+ActiveRecord::Schema.define(version: 20151009003019) do
 
   create_table "backgrounds", force: :cascade do |t|
     t.integer  "attachable_id",      limit: 4
@@ -550,6 +550,13 @@ ActiveRecord::Schema.define(version: 20151008155313) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
   add_index "users", ["slug"], name: "index_users_on_slug", using: :btree
+
+  create_table "video_settings", force: :cascade do |t|
+    t.boolean  "video_platform", default: true
+    t.boolean  "video_upload",   default: true
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
 
   create_table "video_uploads", force: :cascade do |t|
     t.integer  "videoable_id",       limit: 4
