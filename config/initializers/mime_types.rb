be0b6566281f1e0c881a5aab00cb7a1d124e7f0a
Anthony Robin <contact@anthony-robin.fr>
2015-10-11 16:50:39 +0200

    Add mime_types in intializers

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,4 +1,8 @@
-# Be sure to restart your server when you modify this file.
-
-# Add new mime types for use in respond_to blocks:
-# Mime::Type.register "text/richtext", :rtf
+Mime::Type.register 'application/ogg', :ogg
+Mime::Type.register 'application/ogg', :ogx
+Mime::Type.register 'video/ogg', :ogv
+Mime::Type.register 'audio/ogg', :oga
+Mime::Type.register 'video/mp4', :mp4
+Mime::Type.register 'video/mp4', :m4v
+Mime::Type.register 'audio/mpeg', :mp3
+Mime::Type.register 'audio/mpeg', :m4a
