5015ee0060f3d6e9e875c66881ac5ce1c5e1034e
Anthony Robin <contact@anthony-robin.fr>
2015-10-29 21:56:24 +0100

    Refactor prev_next decorator method and move it in application_decorator

--- app/decorators/optional_modules/blogs/blog_setting_decorator.rb
+++ app/decorators/optional_modules/blogs/blog_setting_decorator.rb
@@ -4,9 +4,4 @@
 class BlogSettingDecorator < ApplicationDecorator
   include Draper::LazyHelpers
   delegate_all
-
-  def prev_next
-    color = model.prev_next? ? 'green' : 'red'
-    status_tag_deco I18n.t("enabled.#{model.prev_next}"), color
-  end
 end
