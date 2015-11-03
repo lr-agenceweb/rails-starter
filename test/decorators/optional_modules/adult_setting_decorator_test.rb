ffc5d75f68df9b6625fb0c6caed9419038c974dd
Anthony Robin <contact@anthony-robin.fr>
2015-11-03 21:12:29 +0100

    Add more tests for AdultSettingDecorator

--- test/decorators/optional_modules/adult_setting_decorator_test.rb
+++ test/decorators/optional_modules/adult_setting_decorator_test.rb
@@ -17,6 +17,38 @@ class AdultSettingDecoratorTest < Draper::TestCase
     assert_equal 'http://www.lr-agenceweb.fr', adult_setting_decorated.redirect_link_d
   end
 
+  test 'should return correct title' do
+    adult_setting_decorated = AdultSettingDecorator.new(@adult_setting)
+    assert_equal 'Bienvenue sur le site de démonstration des modules !', adult_setting_decorated.title_d
+
+    I18n.with_locale(:en) do
+      assert_equal 'Welcome to the demonstration website for modules', adult_setting_decorated.title_d
+    end
+  end
+
+  test 'should return correct content' do
+    adult_setting_decorated = AdultSettingDecorator.new(@adult_setting)
+    assert_equal '<p>Venez tester les différents modules</p>', adult_setting_decorated.content_d
+
+    I18n.with_locale(:en) do
+      assert_equal '<p>Come and test all the modules</p>', adult_setting_decorated.content_d
+    end
+  end
+
+  #
+  # == Status tag
+  #
+  test 'should return correct status_tag if enabled' do
+    adult_setting_decorated = AdultSettingDecorator.new(@adult_setting)
+    assert_match "<span class=\"status_tag activé green\">Activé</span>", adult_setting_decorated.status
+  end
+
+  test 'should return correct status_tag if disabled' do
+    @adult_setting.update_attributes(enabled: false)
+    adult_setting_decorated = AdultSettingDecorator.new(@adult_setting)
+    assert_match "<span class=\"status_tag désactivé red\">Désactivé</span>", adult_setting_decorated.status
+  end
+
   private
 
   def initialize_test
