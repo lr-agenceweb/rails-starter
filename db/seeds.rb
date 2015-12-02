9e23cfc3e2513bdcd9dbcc7218567d4b6c6e7664
Anthony Robin <contact@anthony-robin.fr>
2015-12-02 18:06:25 +0100

    Add LegalNotice articles to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -133,7 +133,7 @@ end
 # == Menu
 #
 puts 'Creating Menu elements'
-models_name = [:Home, :Search, :GuestBook, :Blog, :Event, :About, :Contact]
+models_name = [:Home, :Search, :GuestBook, :Blog, :Event, :About, :Contact, :LegalNotice]
 title_en = [
   'Home',
   'Search',
@@ -141,7 +141,8 @@ title_en = [
   'Blog',
   'Events',
   'About',
-  'Contact'
+  'Contact',
+  'Legal notices'
 ]
 title_fr = [
   'Accueil',
@@ -150,7 +151,8 @@ title_fr = [
   'Blog',
   'Événements',
   'À Propos',
-  'Me contacter'
+  'Me contacter',
+  'Mentions légales'
 ]
 show_in_header = [
   true,
@@ -171,7 +173,8 @@ show_in_footer = [
   false
 ]
 
-title_en.each_with_index do |element, index|
+models_name.each_with_index do |element, index|
+  element = element.to_s
   menu = Menu.create!(
     title: title_fr[index],
     show_in_header: show_in_header[index],
@@ -190,9 +193,10 @@ title_en.each_with_index do |element, index|
   @menu_search = menu.id if element == 'Search'
   @menu_guest_book = menu.id if element == 'GuestBook'
   @menu_blog = menu.id if element == 'Blog'
-  @menu_event = menu.id if element == 'Events'
+  @menu_event = menu.id if element == 'Event'
   @menu_about = menu.id if element == 'About'
   @menu_contact = menu.id if element == 'Contact'
+  @menu_legal_notice = menu.id if element == 'LegalNotice'
 end
 
 #
@@ -206,7 +210,8 @@ description_en = [
   'Blog description',
   'Event description',
   'About description',
-  'Contact description'
+  'Contact description',
+  'Legal notices description'
 ]
 description_fr = [
   'Description pour la page d\'accueil',
@@ -215,7 +220,8 @@ description_fr = [
   'Description de la page Blog',
   'Description de la page événement',
   'Description de la page à propos',
-  'Description de la page contact'
+  'Description de la page contact',
+  'Description de la page mentions légales'
 ]
 keywords_en = [
   'home',
@@ -224,7 +230,8 @@ keywords_en = [
   'blog',
   'event',
   'about',
-  'contact'
+  'contact',
+  'legal notices'
 ]
 keywords_fr = [
   'accueil',
@@ -233,7 +240,8 @@ keywords_fr = [
   'blog',
   'événement',
   'à propos',
-  'contact'
+  'contact',
+  'mentions légales'
 ]
 
 models_name.each_with_index do |element, index|
@@ -251,6 +259,7 @@ models_name.each_with_index do |element, index|
   menu_id = @menu_event if element == 'Event'
   menu_id = @menu_about if element == 'About'
   menu_id = @menu_contact if element == 'Contact'
+  menu_id = @menu_legal_notice if element == 'LegalNotice'
 
   category = Category.create!(
     name: element,
@@ -295,6 +304,59 @@ Background.create!(
   attachable_type: 'Category',
   image: File.new("#{Rails.root}/public/system/seeds/backgrounds/background_event.jpg")
 )
+
+#
+# == LegalNotice articles
+#
+puts 'Creating LegalNotice article'
+legal_notice_title_fr = [
+  'Hébergement du site',
+  'Nom de domaine'
+]
+legal_notice_title_en = [
+  'Site hosting',
+  'Domain name'
+]
+legal_notice_slug_fr = [
+  'hebergement-du-site',
+  'nom-de-domaine'
+]
+legal_notice_slug_en = [
+  'site-hosting',
+  'domain-name'
+]
+legal_notice_content_fr = [
+  '<p><strong>Ce site est hébergé par</strong>: <br>  Hébergeur : ONLINE SAS<br>  Adresse web : <a href="https://www.online.net/fr">https://www.online.net</a><br>Téléphone : +33.(0)1.84.13.00.00<br>TVA : FR 35 433115904</p><p><strong>Adresse Postale</strong>: <br>  ONLINE SAS<br>  BP 438 75366<br>  PARIS CEDEX 08</p>',
+  '<p><strong>Le nom de domaine provient de</strong> :<br>  Hébergeur : OVH SAS<br>  Adresse web : <a href="https://www.ovh.com">https://www.ovh.com</a><br>  Téléphone: 1007<br>  SAS au capital de 10 059 500 €<br>  RCS Lille Métropole 424 761 419 00045<br>  Code APE 6202A<br>  N° TVA : FR 22 424 761 419</p><p><strong>Siège social</strong> :<br>  2 rue Kellermann<br>  59100 Roubaix<br>  France</p>'
+]
+legal_notice_content_en = [
+  '<p><strong>This website is hosted by</strong>: <br>  Host : ONLINE SAS<br> Website : <a href="https://www.online.net/fr">https://www.online.net</a><br>Phone : +33.(0)1.84.13.00.00<br>TVA : FR 35 433115904<br></p><p><strong>Postal address</strong>: <br>  ONLINE SAS<br>  BP 438 75366<br>  PARIS CEDEX 08</p>',
+  '<p><strong>The domain name come from</strong> :<br>  Host : OVH SAS<br>Website : <a href="https://www.ovh.com">https://www.ovh.com</a><br>  Phone: 1007<br>  SAS au capital de 10 059 500 €<br>  RCS Lille Métropole 424 761 419 00045<br>  Code APE 6202A<br>  N° TVA : FR 22 424 761 419</p><p><strong>Head office</strong> :<br>  2 rue Kellermann<br>  59100 Roubaix<br>  France</p>'
+]
+legal_notice_user_id = [
+  super_administrator.id,
+  super_administrator.id
+]
+
+legal_notice_title_fr.each_with_index do |element, index|
+  legal_notice = LegalNotice.create!(
+    title: legal_notice_title_fr[index],
+    slug: legal_notice_slug_fr[index],
+    content: legal_notice_content_fr[index],
+    online: true,
+    user_id: legal_notice_user_id[index]
+  )
+
+  if @locales.include?(:en)
+    LegalNotice::Translation.create!(
+      post_id: legal_notice.id,
+      locale: 'en',
+      title: legal_notice_title_en[index],
+      slug: legal_notice_slug_en[index],
+      content: legal_notice_content_en[index]
+    )
+  end
+end
 
 #
 # == Home article
