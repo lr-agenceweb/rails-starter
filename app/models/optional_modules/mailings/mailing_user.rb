56ed72e6976ff92466bf3d00df832439ef5faeea
Anthony Robin <contact@anthony-robin.fr>
2015-12-09 13:44:15 +0100

    Add validation rule for MailingUser email

--- app/models/optional_modules/mailings/mailing_user.rb
+++ app/models/optional_modules/mailings/mailing_user.rb
@@ -22,6 +22,16 @@ class MailingUser < ActiveRecord::Base
   scope :archive, -> { where(archive: true) }
   scope :not_archive, -> { where.not(archive: true) }
 
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            email_format: true
+
+  validates :lang,
+            presence: true,
+            allow_blank: false,
+            inclusion: %w( fr en )
+
   def name
     "#{email} <small>(#{fullname}) #{self.decorate.archive_status}</small>".html_safe
   end
