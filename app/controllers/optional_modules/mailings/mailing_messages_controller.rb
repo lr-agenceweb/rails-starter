39b444c0363cc5f832d2a1e310aa14e95f8cda18
Anthony Robin <contact@anthony-robin.fr>
2015-12-09 20:55:19 +0100

    Add tests for preview_in_browser mailing

---
+++ app/controllers/optional_modules/mailings/mailing_messages_controller.rb
@@ -0,0 +1,43 @@
+#
+# == MailingMessages Controller
+#
+class MailingMessagesController < ApplicationController
+  before_action :not_found, unless: proc { @mailing_module.enabled? }
+  before_action :set_mailing_user, only: [:preview_in_browser]
+  before_action :set_mailing_message, only: [:preview_in_browser]
+  layout 'error'
+
+  def preview_in_browser
+    if all_conditions_respected?
+      @title = @mailing_message.title
+      @content = @mailing_message.content
+      @hide_preview_link = true
+      I18n.with_locale(params[:locale]) do
+        render layout: 'mailing', template: 'mailing_message_mailer/send_email'
+      end
+    else
+      fail ActionController::RoutingError, 'Not Found'
+    end
+  end
+
+  private
+
+  def set_mailing_user
+    @mailing_user = MailingUser.find(params[:mailing_user_id])
+  rescue ActiveRecord::RecordNotFound
+    fail ActionController::RoutingError, 'Not Found'
+  end
+
+  def set_mailing_message
+    @mailing_message = MailingMessage.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    fail ActionController::RoutingError, 'Not Found'
+  end
+
+  def all_conditions_respected?
+    params[:mailing_user_token] &&
+    params[:token] &&
+    @mailing_user.token == params[:mailing_user_token] &&
+    @mailing_message.token == params[:token]
+  end
+end
---
+++ test/controllers/optional_modules/mailings/mailing_messages_controller_test.rb
@@ -0,0 +1,84 @@
+require 'test_helper'
+
+#
+# == MailingMessagesController Test
+#
+class MailingMessagesControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+  include Rails.application.routes.url_helpers
+
+  setup :initialize_test
+
+  #
+  # == Preview in browser
+  #
+  test 'should render preview in browser template' do
+    @locales.each do |locale|
+      I18n.with_locale(locale.to_s) do
+        get :preview_in_browser, locale: locale.to_s, id: @mailing_message.id, token: @mailing_message.token, mailing_user_id: @mailing_user.id, mailing_user_token: @mailing_user.token
+        assert_template :send_email
+        assert_template :send_email, layout: 'mailing'
+      end
+    end
+  end
+
+  #
+  # == Request integrity
+  #
+  test 'should render 404 if tries to access another mailing' do
+    assert_raises(ActionController::RoutingError) do
+      @locales.each do |locale|
+        I18n.with_locale(locale.to_s) do
+          get :preview_in_browser, locale: locale.to_s, id: @mailing_message_two.id, token: @mailing_message.token, mailing_user_id: @mailing_user.id, mailing_user_token: @mailing_user.token
+        end
+      end
+    end
+  end
+
+  test 'should render 404 if mailing_user token is not present' do
+    assert_raises(ActionController::RoutingError) do
+      @locales.each do |locale|
+        I18n.with_locale(locale.to_s) do
+          get :preview_in_browser, locale: locale.to_s, id: @mailing_message_two.id, token: @mailing_message.token, mailing_user_id: @mailing_user.id, mailing_user_token: ''
+        end
+      end
+    end
+  end
+
+  test 'should render 404 if mailing_message token is not present' do
+    assert_raises(ActionController::RoutingError) do
+      @locales.each do |locale|
+        I18n.with_locale(locale.to_s) do
+          get :preview_in_browser, locale: locale.to_s, id: @mailing_message_two.id, token: '', mailing_user_id: @mailing_user.id, mailing_user_token: @mailing_user.token
+        end
+      end
+    end
+  end
+
+  #
+  # == Module disabled
+  #
+  test 'should render 404 if module is disabled' do
+    disable_optional_module @super_administrator, @mailing_module, 'Mailing' # in test_helper.rb
+    assert_raises(ActionController::RoutingError) do
+      @locales.each do |locale|
+        I18n.with_locale(locale.to_s) do
+          get :preview_in_browser, locale: locale.to_s, id: @mailing_message.id, token: @mailing_message.token, mailing_user_id: @mailing_user.id, mailing_user_token: @mailing_user.token
+        end
+      end
+    end
+  end
+
+  private
+
+  def initialize_test
+    @setting = settings(:one)
+    @locales = I18n.available_locales
+    @mailing_user = mailing_users(:one)
+    @mailing_message = mailing_messages(:one)
+    @mailing_message_two = mailing_messages(:two)
+    @mailing_module = optional_modules(:mailing)
+
+    @super_administrator = users(:anthony)
+  end
+end
