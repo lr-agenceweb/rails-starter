9d2f1bc19ebc8cdf9fc14e3f9b0d10a0ca1915b8
Anthony Robin <contact@anthony-robin.fr>
2016-01-05 17:11:48 +0100

    Remove from sitemap pages when menu is offline

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -9,9 +9,9 @@ SitemapGenerator::Sitemap.sitemaps_path = 'shared/'
 SitemapGenerator::Sitemap.create do
   group(sitemaps_path: 'sitemap/fr/', filename: :french) do
     classic_links
-    blog_module if OptionalModule.by_name('Blog').enabled?
-    event_module if OptionalModule.by_name('Event').enabled?
-    guest_book_module if OptionalModule.by_name('GuestBook').enabled?
+    blog_module if OptionalModule.by_name('Blog').enabled? && Category.find_by(name: 'Blog').menu_online
+    event_module if OptionalModule.by_name('Event').enabled? && Category.find_by(name: 'Event').menu_online
+    guest_book_module if OptionalModule.by_name('GuestBook').enabled? && Category.find_by(name: 'GuestBook').menu_online
     rss_module if OptionalModule.by_name('RSS').enabled?
   end
 
@@ -19,9 +19,9 @@ SitemapGenerator::Sitemap.create do
     group(sitemaps_path: 'sitemap/en/', filename: :english) do
       I18n.with_locale(:en) do
         classic_links
-        blog_module if OptionalModule.by_name('Blog').enabled?
-        event_module if OptionalModule.by_name('Event').enabled?
-        guest_book_module if OptionalModule.by_name('GuestBook').enabled?
+        blog_module if OptionalModule.by_name('Blog').enabled? && Category.find_by(name: 'Blog').menu_online
+        event_module if OptionalModule.by_name('Event').enabled? && Category.find_by(name: 'Event').menu_online
+        guest_book_module if OptionalModule.by_name('GuestBook').enabled? && Category.find_by(name: 'GuestBook').menu_online
         rss_module if OptionalModule.by_name('RSS').enabled?
       end
     end
