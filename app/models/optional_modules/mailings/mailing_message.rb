9415ebeb8876c8ea556a5171945c985681054fc8
Anthony Robin <contact@anthony-robin.fr>
2016-01-24 16:40:46 +0100

    Redirect to update action after uploading a picture to a MailingMessage

--- app/models/optional_modules/mailings/mailing_message.rb
+++ app/models/optional_modules/mailings/mailing_message.rb
@@ -23,6 +23,15 @@ class MailingMessage < ActiveRecord::Base
   translates :title, :content, fallbacks_for_empty_translations: true
   active_admin_translates :title, :content
 
+  attr_accessor :should_redirect
+  before_save :redirect_to_form?, if: proc { |r| r.picture.try(:new_record?) || r.picture.try(:changed?) }
+
   has_many :mailing_users, through: :mailing_message_users
   has_many :mailing_message_users, dependent: :destroy
+
+  private
+
+  def redirect_to_form?
+    self.should_redirect = true
+  end
 end
