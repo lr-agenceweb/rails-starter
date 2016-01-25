b14078e99a1d053fa233ad5214cd11bc01bc3031
Anthony Robin <contact@anthony-robin.fr>
2016-01-25 18:32:11 +0100

    Add support for srt mimetype

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,8 +1,16 @@
-Mime::Type.register 'application/ogg', :ogg
-Mime::Type.register 'application/ogg', :ogx
-Mime::Type.register 'video/ogg', :ogv
-Mime::Type.register 'audio/ogg', :oga
+# Video
 Mime::Type.register 'video/mp4', :mp4
 Mime::Type.register 'video/mp4', :m4v
+Mime::Type.register 'video/webm', :webm
+Mime::Type.register 'video/ogg', :ogv
+Mime::Type.register 'application/ogg', :ogg
+Mime::Type.register 'application/ogg', :ogx
+
+# Audio
 Mime::Type.register 'audio/mpeg', :mp3
 Mime::Type.register 'audio/mpeg', :m4a
+Mime::Type.register 'audio/ogg', :oga
+
+# Subtitle
+Mime::Type.register 'text/srt', :srt
+Mime::Type.register 'text/vtt', :vtt
