2dbecdb333da319ea945fedfd8ff7d7e8ca1c7a2
Anthony Robin <contact@anthony-robin.fr>
2016-02-08 12:10:53 +0100

    Refactor way to init decorator for MailingMessage

--- test/decorators/optional_modules/mailings/mailing_message_decorator_test.rb
+++ test/decorators/optional_modules/mailings/mailing_message_decorator_test.rb
@@ -9,21 +9,20 @@ class MailingMessageDecoratorTest < Draper::TestCase
   setup :initialize_test
 
   test 'should return correct value for sent_at method' do
-    mailing_message_decorated = MailingMessageDecorator.new(@mailing_message)
-    assert_equal 'Envoyée le mercredi 09 décembre 2015 à 18h10', mailing_message_decorated.sent_at_message
+    assert_equal 'Envoyée le mercredi 09 décembre 2015 à 18h10', @mailing_message_decorated.sent_at_message
   end
 
   #
   # == Status tag
   #
   test 'should return correct status_tag for sent attribute' do
-    mailing_message_decorated = MailingMessageDecorator.new(@mailing_message)
-    assert_match "<span class=\"status_tag envoyé red\">Envoyé</span>", mailing_message_decorated.status
+    assert_match "<span class=\"status_tag envoyé red\">Envoyé</span>", @mailing_message_decorated.status
   end
 
   private
 
   def initialize_test
     @mailing_message = mailing_messages(:one)
+    @mailing_message_decorated = MailingMessageDecorator.new(@mailing_message)
   end
 end
