79cc6c3e6546933c2f17d8bc4e4fff71ec98a71c
Anthony Robin <contact@anthony-robin.fr>
2016-02-08 12:24:00 +0100

    Add tests for NewsletterDecorator

--- app/decorators/optional_modules/newsletters/newsletter_decorator.rb
+++ app/decorators/optional_modules/newsletters/newsletter_decorator.rb
@@ -5,8 +5,15 @@ class NewsletterDecorator < ApplicationDecorator
   include Draper::LazyHelpers
   delegate_all
 
+  #
+  # == Content
+  #
   def title
     model.title if title?
+  end
+
+  def sent_at
+    model.sent_at_message
   end
 
   def preview
@@ -22,20 +29,19 @@ class NewsletterDecorator < ApplicationDecorator
     render '/admin/newsletters/iframe_preview', resource: model
   end
 
-  def sent_at
-    model.sent_at_message
-  end
-
   def send_link
     render '/admin/newsletters/send', resource: model
   end
 
+  def list_subscribers
+    render '/admin/newsletters/list_subscribers', newsletter_users: NewsletterUser.all
+  end
+
+  #
+  # == Status tag
+  #
   def status
     color = model.already_sent? ? 'red' : 'green'
     status_tag_deco(I18n.t("sent.#{model.already_sent?}"), color)
-  end
-
-  def list_subscribers
-    render '/admin/newsletters/list_subscribers', newsletter_users: NewsletterUser.all
   end
 end
