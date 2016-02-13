abf0a9db290c159795f73020ff4c9e57edfb9c5e
Anthony Robin <contact@anthony-robin.fr>
2016-02-13 14:54:43 +0100

    Fix small bug with date format after a refactoring

--- test/decorators/optional_modules/comments/comment_decorator_test.rb
+++ test/decorators/optional_modules/comments/comment_decorator_test.rb
@@ -37,24 +37,24 @@ class CommentDecoratorTest < Draper::TestCase
   # == Avatar
   #
   test 'should return correct avatar for connected user' do
-    assert_equal "<img alt=\"anthony\" src=\"http://gravatar.com/avatar/d7097d5b6b00db57b0bf772923729a7d?secure=false\" width=\"80\" height=\"80\" />", @comment_decorated.avatar
+    assert_equal "<img alt=\"anthony\" src=\"http://gravatar.com/avatar/d7097d5b6b00db57b0bf772923729a7d?default=mm&secure=false\" width=\"80\" height=\"80\" />", @comment_decorated.avatar
   end
 
   test 'should return correct avatar for guest user' do
     comment_decorated = CommentDecorator.new(@comment_not_connected)
-    assert_equal "<img alt=\"luke\" src=\"http://gravatar.com/avatar/2e5c8c61be4beb99af2f3c5fbb77e988?secure=false\" width=\"80\" height=\"80\" />", comment_decorated.avatar
+    assert_equal "<img alt=\"luke\" src=\"http://gravatar.com/avatar/2e5c8c61be4beb99af2f3c5fbb77e988?default=mm&secure=false\" width=\"80\" height=\"80\" />", comment_decorated.avatar
   end
 
   test 'should return correct author with avatar' do
     comment_decorated = CommentDecorator.new(@comment_not_connected)
-    assert_equal "<div class=\"author-with-avatar\"><img alt=\"luke\" src=\"http://gravatar.com/avatar/2e5c8c61be4beb99af2f3c5fbb77e988?secure=false\" width=\"80\" height=\"80\" /> <br /> Luke</div>", comment_decorated.author_with_avatar
+    assert_equal "<div class=\"author-with-avatar\"><img alt=\"luke\" src=\"http://gravatar.com/avatar/2e5c8c61be4beb99af2f3c5fbb77e988?default=mm&secure=false\" width=\"80\" height=\"80\" /> <br /> Luke</div>", comment_decorated.author_with_avatar
   end
 
   #
   # == Date
   #
   test 'should return correct date of creation for comment' do
-    assert_equal '<small><time datetime="2016-01-30T13:54:20+01:00">samedi 30 janvier 2016</time></small>', @comment_decorated.comment_created_at
+    assert_equal '<small><time datetime="2016-01-30T13:54:20+01:00">samedi 30 janvier 2016 13:54</time></small>', @comment_decorated.comment_created_at
   end
 
   #
