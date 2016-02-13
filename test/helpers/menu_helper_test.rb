e1f85326665e48a7950ddd98693182db7a2900b1
Anthony Robin <contact@anthony-robin.fr>
2016-02-13 15:08:32 +0100

    Add test for MenuHelper

--- test/helpers/menu_helper_test.rb
+++ test/helpers/menu_helper_test.rb
@@ -8,24 +8,29 @@ class MenuHelperTest < ActionView::TestCase
 
   setup :initialize_test
 
-  test 'should return active class for home page' do
+  test 'should return active class for home' do
     assert_equal set_active_class('homes', 'index'), 'active'
   end
 
-  test 'should not return active class if action params is show for home page' do
+  test 'should not return active class if action is show for home' do
     assert_nil set_active_class('homes', 'show')
   end
 
-  test 'should return active class if action params is false for home page' do
+  test 'should return active class if action is false for home' do
     assert_equal set_active_class('homes'), 'active'
   end
 
-  test 'should not return active class for contact page' do
+  test 'should not return active class for contact' do
     assert_nil set_active_class('contacts', 'new')
   end
 
-  test 'should not return active class if action params is false for contact page' do
+  test 'should not return active class if action is false for contact' do
     assert_nil set_active_class('contacts')
+  end
+
+  test 'should return correct even or odd class for menu' do
+    items = Menu.online.only_parents.with_page.with_allowed_modules.visible_header
+    assert_equal 'even', even_or_odd_menu_item(items)
   end
 
   private
