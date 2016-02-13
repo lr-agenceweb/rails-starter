7dc0cf6980f888db01fd5d977f34262bcad3d4e4
Anthony Robin <contact@anthony-robin.fr>
2016-02-13 12:19:35 +0100

    Add test for ActiveAdminHelper

--- app/helpers/active_admin/active_admin_helper.rb
+++ app/helpers/active_admin/active_admin_helper.rb
@@ -8,7 +8,7 @@ module ActiveAdmin
   module ActiveAdminHelper
     def edit_heading_page_aa
       category = Category.find_by(name: controller_name.classify)
-      link_to t('active_admin.action_item.edit_heading_page', page: category.menu_title), edit_admin_category_path(category)
+      link_to t('active_admin.action_item.edit_heading_page', page: category.menu_title).html_safe, edit_admin_category_path(category)
     end
   end
 end
