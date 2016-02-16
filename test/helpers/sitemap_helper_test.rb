6c4717535545bedbcc1bb2cd9b8b7fd895088d80
Anthony Robin <contact@anthony-robin.fr>
2016-02-16 19:50:21 +0100

    Add test file for SitemapHelper

---
+++ test/helpers/sitemap_helper_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+#
+# == SitemapHelper Test
+#
+class SitemapHelperTest < ActionView::TestCase
+  include Rails.application.routes.url_helpers
+
+  test 'should return correct rss_module content' do
+    skip 'Add method is not understood by the test'
+  end
+end
