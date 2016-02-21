93b20780611899cec3213ff568263ca71d0c00d2
Anthony Robin <contact@anthony-robin.fr>
2016-02-21 13:56:33 +0100

    Add tests for SocialProvider validations rules

--- test/models/optional_modules/social_connects/social_provider_test.rb
+++ test/models/optional_modules/social_connects/social_provider_test.rb
@@ -4,7 +4,33 @@ require 'test_helper'
 # == SocialProvider test
 #
 class SocialProviderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup :initialize_test
+
+  #
+  # == Validation
+  #
+  test 'should save social provider if all good' do
+    @facebook_provider.destroy
+    social_provider = SocialProvider.new(name: 'facebook')
+    assert social_provider.valid?
+    assert_empty social_provider.errors.keys
+  end
+
+  test 'should not save social provider if not unique' do
+    social_provider = SocialProvider.new(name: 'facebook')
+    assert_not social_provider.valid?
+    assert_equal [:name], social_provider.errors.keys
+  end
+
+  test 'should not save social provider if not allowed' do
+    social_provider = SocialProvider.new(name: 'Linkedin')
+    assert_not social_provider.valid?
+    assert_equal [:name], social_provider.errors.keys
+  end
+
+  private
+
+  def initialize_test
+    @facebook_provider = social_providers(:facebook)
+  end
 end
