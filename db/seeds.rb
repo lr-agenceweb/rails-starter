8d1fb3df7ddd9d99870488b977c504fc6f2948df
Anthony Robin <contact@anthony-robin.fr>
2016-02-29 15:37:44 +0100

    Delete public/system/<env> folder before seeding database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,12 @@ require 'database_cleaner'
 DatabaseCleaner.strategy = :truncation
 DatabaseCleaner.clean
 
+dir = "#{Rails.root}/public/system/#{Rails.env}"
+if Dir.exist?(dir)
+  puts "Delete public/system/#{Rails.env} folder"
+  FileUtils.rm_rf(dir)
+end
+
 #
 # == Create user roles
 #
