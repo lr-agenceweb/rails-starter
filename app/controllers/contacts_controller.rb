0148ff5c2c5bc11ee55ed117f150387ce1aecc82
Anthony Robin <contact@anthony-robin.fr>
2016-03-06 00:57:09 +0100

    Remove unwanted annotation on controllers

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,26 +1,3 @@
-# == Schema Information
-#
-# Table name: posts
-#
-#  id              :integer          not null, primary key
-#  type            :string(255)
-#  title           :string(255)
-#  slug            :string(255)
-#  content         :text(65535)
-#  show_as_gallery :boolean          default(FALSE)
-#  allow_comments  :boolean          default(TRUE)
-#  online          :boolean          default(TRUE)
-#  position        :integer
-#  user_id         :integer
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#
-# Indexes
-#
-#  index_posts_on_slug     (slug) UNIQUE
-#  index_posts_on_user_id  (user_id)
-#
-
 #
 # == ContactsController
 #
