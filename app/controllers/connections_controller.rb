9acfc30642bb85c05cfa46ff3811b0038325b702
Anthony Robin <contact@anthony-robin.fr>
2016-03-29 01:20:26 +0200

    Setup controller and decortor for Connection resource

---
+++ app/controllers/connections_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+#
+# == Connections Controller
+#
+class ConnectionsController < PostsController
+  decorates_assigned :connection
+
+  # GET /liens
+  # GET /liens.json
+  def index
+    @connections = Connection.includes(:translations).online.by_position
+    per_p = @setting.per_page == 0 ? @connections.count : @setting.per_page
+    @connections = ConnectionDecorator.decorate_collection(@connections.page(params[:page]).per(per_p))
+    seo_tag_index category
+  end
+end
---
+++ test/decorators/posts/connection_decorator_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+#
+# == ConnectionDecorator test
+#
+class ConnectionDecoratorTest < Draper::TestCase
+end
