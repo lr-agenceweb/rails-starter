ef2c10b21b830a75406032b4b260afcd7bfa4fe4
Anthony Robin <contact@anthony-robin.fr>
2016-03-30 01:04:13 +0200

    Add Connection model tests to ensure url validation rule

--- test/models/posts/connection_test.rb
+++ test/models/posts/connection_test.rb
@@ -5,4 +5,18 @@ require 'test_helper'
 # == Connection model test
 #
 class ConnectionTest < ActiveSupport::TestCase
+  #
+  # == Validation rules
+  #
+  test 'should not create connection if link is not correct' do
+    connection = Connection.new link_attributes: { url: 'bad-link' }
+    assert_not connection.valid?
+    assert_equal [:'link.url'], connection.errors.keys
+  end
+
+  test 'should create connection if link is correct' do
+    connection = Connection.new link_attributes: { url: 'http://test.com' }
+    assert connection.valid?
+    assert_empty connection.errors.keys
+  end
 end
