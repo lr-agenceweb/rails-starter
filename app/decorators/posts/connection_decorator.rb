8499593a522f2860644aef3abf98505b136f88fe
Anthony Robin <contact@anthony-robin.fr>
2016-03-29 18:55:22 +0200

    Create AA Connection resource

--- app/decorators/posts/connection_decorator.rb
+++ app/decorators/posts/connection_decorator.rb
@@ -5,4 +5,14 @@
 class ConnectionDecorator < PostDecorator
   include Draper::LazyHelpers
   delegate_all
+
+  def link
+    model.link.url if link?
+  end
+
+  private
+
+  def link?
+    model.link.try(:url).present?
+  end
 end
--- app/models/concerns/linkable.rb
+++ app/models/concerns/linkable.rb
@@ -5,6 +5,8 @@
 module Linkable
   extend ActiveSupport::Concern
 
-  has_one :link, as: :linkable, dependent: :destroy
-  accepts_nested_attributes_for :link, reject_if: :all_blank, allow_destroy: true
+  included do
+    has_one :link, as: :linkable, dependent: :destroy
+    accepts_nested_attributes_for :link, reject_if: :all_blank, allow_destroy: true
+  end
 end
