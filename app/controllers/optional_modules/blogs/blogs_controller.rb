b347f3e69f3c3631ad0d04422b97ca2f35b2e8a5
Anthony Robin <contact@anthony-robin.fr>
2016-05-04 16:59:01 +0200

    Fix N+1 query with blogs objects

--- app/controllers/optional_modules/blogs/blogs_controller.rb
+++ app/controllers/optional_modules/blogs/blogs_controller.rb
@@ -13,7 +13,7 @@ class BlogsController < ApplicationController
   # GET /blog
   # GET /blog.json
   def index
-    @blogs = Blog.includes(:translations, :user).online.order(created_at: :desc)
+    @blogs = Blog.includes(:translations, :user, :blog_category, :picture, :video_uploads, :video_platforms).online.order(created_at: :desc)
     per_p = @setting.per_page == 0 ? @blogs.count : @setting.per_page
     @blogs = BlogDecorator.decorate_collection(@blogs.page(params[:page]).per(per_p))
     seo_tag_index category
