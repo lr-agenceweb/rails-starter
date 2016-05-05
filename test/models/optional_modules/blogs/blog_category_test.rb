64c15363f22f15216317b61ebd1fac2b6e054bff
Anthony Robin <contact@anthony-robin.fr>
2016-05-05 16:49:27 +0200

    Add tests to ensure globalize validation rules are working for BlogCategory

--- test/models/optional_modules/blogs/blog_category_test.rb
+++ test/models/optional_modules/blogs/blog_category_test.rb
@@ -9,29 +9,46 @@ class BlogCategoryTest < ActiveSupport::TestCase
   # == Validation rules
   #
   test 'should not be valid if name is not set' do
-    blog_category = BlogCategory.new
+    blog_category = BlogCategory.new(id: SecureRandom.uuid)
+    blog_category.set_translations(
+      fr: {},
+      en: {}
+    )
     refute blog_category.valid?, 'should not be valid if name is not set'
-    assert_equal [:name], blog_category.errors.keys
+    assert_equal [:'translations.name'], blog_category.errors.keys
   end
 
   test 'should not be valid if name is empty' do
-    attrs = { name: '' }
-    blog_category = BlogCategory.new attrs
+    blog_category = BlogCategory.new(id: SecureRandom.uuid)
+    blog_category.set_translations(
+      fr: { name: '' },
+      en: { name: '' }
+    )
     refute blog_category.valid?, 'should not be valid if name is empty'
-    assert_equal [:name], blog_category.errors.keys
+    assert_equal [:'translations.name'], blog_category.errors.keys
   end
 
   test 'should not be valid if name is already taken' do
-    attrs = { name: 'foo' }
-    blog_category = BlogCategory.new attrs
+    blog_category = BlogCategory.new(id: SecureRandom.uuid)
+    blog_category.set_translations(
+      fr: { name: 'foo' },
+      en: { name: 'bar' }
+    )
     refute blog_category.valid?, 'should not be valid if name is already taken'
-    assert_equal [:name], blog_category.errors.keys
+    assert_equal [:'translations.name'], blog_category.errors.keys
   end
 
   test 'should be valid if name is set properly' do
-    attrs = { name: 'foobar' }
-    blog_category = BlogCategory.new attrs
+    blog_category = BlogCategory.new(id: SecureRandom.uuid)
+    blog_category.set_translations(
+      fr: { name: 'foofoo' },
+      en: { name: 'barbar' }
+    )
+
     assert blog_category.valid?, 'should be valid if name is set properly'
     assert_empty blog_category.errors.keys
+
+    I18n.with_locale('fr') { assert_equal 'foofoo', blog_category.name }
+    I18n.with_locale('en') { assert_equal 'barbar', blog_category.name }
   end
 end
