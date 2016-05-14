897763d3c9adb01ce9fef2a38539574d52c3ed40
Anthony Robin <contact@anthony-robin.fr>
2016-05-14 10:43:20 +0200

    Add AdminBarable concern to define admin_bar variables

---
+++ app/controllers/concerns/optional_modules/admin_barable.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+#
+# == OptionalModules namespace
+#
+module OptionalModules
+  #
+  # == AdminBarableConcern
+  #
+  module AdminBarable
+    extend ActiveSupport::Concern
+
+    included do
+      before_action :admin_bar_enabled?
+      before_action :set_comments_count, if: proc { @comment_module.enabled? && @admin_bar_enabled }
+      before_action :set_guest_books_count, if: proc { @guest_book_module.enabled? && @admin_bar_enabled }
+
+      private
+
+      def admin_bar_enabled?
+        @admin_bar_enabled = current_user_and_administrator?(current_user) && @setting.show_admin_bar? ? true : false
+      end
+
+      def set_comments_count
+        @comments_to_validate_count = Comment.select(:id).to_validate.count
+        @comments_signalled_count = Comment.select(:id).signalled.count
+      end
+
+      def set_guest_books_count
+        @guest_books_to_validate_count = GuestBook.select(:id).to_validate.count
+      end
+    end
+  end
+end
