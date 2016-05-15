9fb5fcb6d7f74e40373c0973b295db47edcf3ec3
Anthony Robin <contact@anthony-robin.fr>
2016-05-15 03:01:03 +0200

    Refactor and improve way to check if flash notice should be shown for Audio resource

--- app/controllers/concerns/optional_modules/audioable.rb
+++ app/controllers/concerns/optional_modules/audioable.rb
@@ -16,14 +16,25 @@ module OptionalModules
       private
 
       def set_audio_flash_notice
-        # Audio object
-        if defined?(resource) && resource.is_a?(Audio) && defined?(resource.audio_flash_notice) && !resource.audio_flash_notice.blank?
+        if audio_object?
           (flash[:notice] ||= []) << resource.audio_flash_notice
-
-        # Audioable relation object
-        elsif defined?(resource.audio.audio_flash_notice) && !resource.audio.audio_flash_notice.blank?
+        elsif audioable_object?
           (flash[:notice] ||= []) << resource.audio.audio_flash_notice
         end
+      end
+
+      def audio_object?
+        # Audio object
+        defined?(resource) &&
+          resource.is_a?(Audio) &&
+          defined?(resource.audio_flash_notice) &&
+          !resource.audio_flash_notice.blank?
+      end
+
+      def audioable_object?
+        # Audioable relation object
+        defined?(resource.audio.audio_flash_notice) &&
+          !resource.audio.audio_flash_notice.blank?
       end
     end
   end
--- app/models/concerns/optional_modules/assets/audio_notifiable.rb
+++ app/models/concerns/optional_modules/assets/audio_notifiable.rb
@@ -16,19 +16,19 @@ module OptionalModules
 
       included do
         attr_accessor :audio_flash_notice
-        after_commit :audio_flash_upload_in_progress
+        after_commit :audio_flash_upload_in_progress, if: :upload_in_progress?
 
         private
 
         def audio_flash_upload_in_progress(*)
-          # Audio object
-          if defined?(resource) && resource.is_a?(Audio)
-            self.audio_flash_notice = I18n.t('audio.flash.upload_in_progress') if !resource.nil? && resource.audio_processing? && !resource.destroyed?
+          self.audio_flash_notice = I18n.t('audio.flash.upload_in_progress')
+        end
 
-          # self Audio object
-          elsif is_a?(Audio) && !nil? && audio_processing? && !destroyed?
-            self.audio_flash_notice = I18n.t('audio.flash.upload_in_progress')
-          end
+        def upload_in_progress?
+          # Audio object
+          (defined?(resource) && resource.is_a?(Audio) && !resource.nil? && resource.audio_processing? && !resource.destroyed?) ||
+            # self Audio object
+            (is_a?(Audio) && !nil? && audio_processing? && !destroyed?)
         end
       end
     end
