fa78bf1cba73bf0e3ba2d15bc28de4a98d4f5b24
Anthony Robin <contact@anthony-robin.fr>
2016-05-24 00:33:02 +0200

    Update current_or_coming Event scope to handle one day events as well

--- app/models/optional_modules/events/event.rb
+++ app/models/optional_modules/events/event.rb
@@ -48,7 +48,7 @@ class Event < ActiveRecord::Base
   delegate :address, :postcode, :city, to: :location, prefix: true, allow_nil: true
 
   scope :online, -> { where(online: true) }
-  scope :current_or_coming, -> { where('(start_date <= ? AND end_date is ?) OR (start_date >= ?) OR (start_date <= ? AND end_date >= ?)', Time.zone.now, nil, Time.zone.now, Time.zone.now, Time.zone.now) }
+  scope :current_or_coming, -> { where('(start_date >= ?) OR (start_date <= ? AND end_date >= ?) OR (start_date = ? AND all_day = ?)', Time.zone.now, Time.zone.now, Time.zone.now, Time.zone.today, true) }
 
   def calendar_date_correct?
     return true unless end_date <= start_date
