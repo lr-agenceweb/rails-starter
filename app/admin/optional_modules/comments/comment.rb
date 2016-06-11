509acc82dcf15e8ed8d60920bc340e9469666356
Anthony Robin <contact@anthony-robin.fr>
2016-06-11 11:14:57 +0200

    Reorder scope and batch action for AA Comment resource, add tests to ensure destroyed batch action doesn't destroy not allowed comments

--- app/admin/optional_modules/comments/comment.rb
+++ app/admin/optional_modules/comments/comment.rb
@@ -12,11 +12,16 @@ ActiveAdmin.register Comment, as: 'PostComment' do
                 :validated,
                 :signalled
 
-  scope I18n.t('scope.all'), :all, default: true, if: proc { current_user_and_administrator? }
-  scope I18n.t('active_admin.globalize.language.fr'), :french, if: proc { @locales.length > 1 && current_user_and_administrator? }
-  scope I18n.t('active_admin.globalize.language.en'), :english, if: proc { @locales.length > 1 && current_user_and_administrator? }
-  scope I18n.t('comment.to_validate.scope'), :to_validate, if: proc { @comment_setting.should_validate? && current_user_and_administrator? }
-  scope I18n.t('comment.signalled.scope'), :signalled, if: proc { @comment_setting.should_signal? && current_user_and_administrator? }
+  scope I18n.t('scope.all'), :all, default: true,
+        if: proc { current_user_and_administrator? }
+  scope I18n.t('active_admin.globalize.language.fr'), :french,
+        if: proc { @locales.length > 1 && current_user_and_administrator? }
+  scope I18n.t('active_admin.globalize.language.en'), :english,
+        if: proc { @locales.length > 1 && current_user_and_administrator? }
+  scope I18n.t('comment.to_validate.scope'), :to_validate,
+        if: proc { @comment_setting.should_validate? && current_user_and_administrator? }
+  scope I18n.t('comment.signalled.scope'), :signalled,
+        if: proc { @comment_setting.should_signal? && current_user_and_administrator? }
 
   decorate_with CommentDecorator
   config.clear_sidebar_sections!
@@ -32,16 +37,25 @@ ActiveAdmin.register Comment, as: 'PostComment' do
     redirect_to :back, notice: t('active_admin.toggle_validated.flash', verb: resource.validated? ? 'validé' : 'invalidé')
   end
 
-  batch_action :toggle_validated, if: proc { can? :toggle_validated, Comment } do |ids|
-    Comment.find(ids).each do |comment|
-      comment.toggle! :validated
-      email_comment_validated(comment) if comment.validated?
+  batch_action :toggle_validated, priority: 1,
+               if: proc { can? :toggle_validated, Comment } do |ids|
+    Comment.find(ids).each do |item|
+      item.toggle! :validated
+      email_comment_validated(item) if item.validated?
     end
     redirect_to :back, notice: t('active_admin.batch_actions.flash')
   end
 
-  batch_action :toggle_signalled, if: proc { can? :toggle_signalled, Comment } do |ids|
+  batch_action :toggle_signalled, priority: 2,
+               if: proc { can? :toggle_signalled, Comment } do |ids|
     Comment.find(ids).each { |item| item.toggle! :signalled }
+    redirect_to :back, notice: t('active_admin.batch_actions.flash')
+  end
+
+  # Override destroy batch_action
+  batch_action :destroy, priority: 3,
+               confirm: I18n.t('active_admin.destroy.confirm', object_kind: I18n.t('activerecord.models.comment.one')) do |ids|
+    Comment.find(ids).each { |item| item.destroy if can? :destroy, item }
     redirect_to :back, notice: t('active_admin.batch_actions.flash')
   end
 
