70db6ad8cd01bb7b4aa6ab958ae2e77c73012f7e
Anthony Robin <contact@anthony-robin.fr>
2016-09-15 00:06:09 +0200

    Codeclimate :: Refactor duplicated code

--- app/mailers/contact_form_mailer.rb
+++ app/mailers/contact_form_mailer.rb
@@ -15,26 +15,25 @@ class ContactFormMailer < ApplicationMailer
       attachments[attachment_name] = message.attachment.read
     end
 
-    mail from: @message.email,
-         to: @setting.email,
-         subject: default_i18n_subject(site: @setting.title, locale: I18n.default_locale),
-         body: @message.message do |format|
-      format.html
-      format.text
-    end
+    mail_method(@message.email, @setting.email)
   end
 
   # Administrator => Customer
   def send_copy(message)
     @message = message
     @copy_to_sender = true
+    mail_method(@setting.email, @message.email)
+  end
 
-    mail from: @setting.email,
-         to: @message.email,
+  private
+
+  def mail_method(from, to, template = :message_me)
+    mail from: from,
+         to: to,
          subject: default_i18n_subject(site: @setting.title, locale: I18n.default_locale),
          body: @message.message do |format|
-      format.html { render :message_me }
-      format.text { render :message_me }
+      format.html { render template }
+      format.text { render template }
     end
   end
 end
--- lib/mailer_previews/contact_form_mailer_preview.rb
+++ lib/mailer_previews/contact_form_mailer_preview.rb
@@ -6,13 +6,7 @@
 #
 class ContactFormMailerPreview < ActionMailer::Preview
   def message_me_preview
-    attrs = {
-      name: 'cristiano',
-      email: 'cristiano@ronaldo.pt',
-      message: 'Hi, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos minus atque accusantium quos perspiciatis dolor itaque dicta nulla architecto ut quaerat sequi nostrum, earum magni doloribus maxime tempore quam sapiente.'
-    }
-
-    @message = ContactForm.new attrs
+    @message = ContactForm.new default_attrs('cristiano', 'cristiano@ronaldo.pt')
 
     I18n.with_locale(:fr) do
       ContactFormMailer.message_me(@message)
@@ -20,16 +14,20 @@ class ContactFormMailerPreview < ActionMailer::Preview
   end
 
   def send_copy_preview
-    attrs = {
-      name: 'Karim',
-      email: 'karim@benzema.fr',
-      message: 'Hi, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos minus atque accusantium quos perspiciatis dolor itaque dicta nulla architecto ut quaerat sequi nostrum, earum magni doloribus maxime tempore quam sapiente.'
-    }
-
-    @message = ContactForm.new attrs
+    @message = ContactForm.new default_attrs('Karim', 'karim@benzema.fr')
 
     I18n.with_locale(:fr) do
       ContactFormMailer.send_copy(@message)
     end
+  end
+
+  private
+
+  def default_attrs(name, email)
+    attrs = {
+      name: name,
+      email: email,
+      message: 'Hi, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos minus atque accusantium quos perspiciatis dolor itaque dicta nulla architecto ut quaerat sequi nostrum, earum magni doloribus maxime tempore quam sapiente.'
+    }
   end
 end
