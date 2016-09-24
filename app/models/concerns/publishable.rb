0686d2e00602e6dac6a941b93cd829574b3efeb3
Anthony Robin <contact@anthony-robin.fr>
2016-09-24 12:39:54 +0200

    Publishable :: Add comments to separate model relations from delegates

--- app/models/concerns/publishable.rb
+++ app/models/concerns/publishable.rb
@@ -7,9 +7,11 @@ module Publishable
   extend ActiveSupport::Concern
 
   included do
+    # Model relations
     has_one :publication_date, as: :publishable, dependent: :destroy
     accepts_nested_attributes_for :publication_date, reject_if: :all_blank, allow_destroy: true
 
+    # Delegates
     delegate :published_later, :published_at,
              :expired_prematurely, :expired_at,
              :published_later?, :expired_prematurely?,
