1e39ef69127cb58ebbe603e0a9731ceae87d8837
Anthony Robin <contact@anthony-robin.fr>
2016-10-17 13:36:42 +0200

    Slider :: Add tests for validation rules

--- test/models/optional_modules/slider_test.rb
+++ test/models/optional_modules/slider_test.rb
@@ -18,6 +18,45 @@ class SliderTest < ActiveSupport::TestCase
     assert_not_includes allowed_animations, 'popup'
   end
 
+  test 'should not be valid if all good' do
+    slider = Slider.new sliders_attrs
+    assert slider.valid?
+    assert_empty slider.errors.keys
+  end
+
+  test 'should not be valid if all empty' do
+    slider = Slider.new {}
+    assert_not slider.valid?
+    assert_equal [:category, :animate], slider.errors.keys
+  end
+
+  test 'should not be valid if time_to_show is not empty' do
+    attrs = sliders_attrs
+    attrs[:time_to_show] = ''
+
+    slider = Slider.new attrs
+    assert_not slider.valid?
+    assert_equal [:time_to_show], slider.errors.keys
+  end
+
+  test 'should not be valid if category is not present' do
+    attrs = sliders_attrs
+    attrs[:category] = nil
+
+    slider = Slider.new attrs
+    assert_not slider.valid?
+    assert_equal [:category], slider.errors.keys
+  end
+
+  test 'should not be valid if animations is not allowed' do
+    attrs = sliders_attrs
+    attrs[:animate] = 'popup'
+
+    slider = Slider.new attrs
+    assert_not slider.valid?
+    assert_equal [:animate], slider.errors.keys
+  end
+
   #
   # == Count
   #
@@ -33,5 +72,13 @@ class SliderTest < ActiveSupport::TestCase
 
   def initialize_test
     @slider = sliders(:online)
+  end
+
+  def sliders_attrs
+    {
+      time_to_show: 3000,
+      category: categories(:blog),
+      animate: 'crossfade'
+    }
   end
 end
