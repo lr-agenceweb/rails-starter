5defbf383a4330c447d29f8eff711e7b3db7ce1d
Anthony Robin <contact@anthony-robin.fr>
2016-11-14 18:40:03 +0100

    FriendlyId/slug :: Fix broken tests

--- app/models/concerns/core/friendly_globalize_sluggable.rb
+++ app/models/concerns/core/friendly_globalize_sluggable.rb
@@ -24,25 +24,27 @@ module Core
       extend FriendlyId
       friendly_id :slug_candidates,
                   use: [:slugged,
-                        :history,
                         :globalize,
+                        # :history,
                         :finders]
 
       private
 
       def slug_candidates
-        [[ATTRIBUTE, :deduced_id]]
+        [
+          ATTRIBUTE,
+          [ATTRIBUTE, resource_id]
+        ]
       end
 
-      def deduced_id
-        record_id = self.class.where("#{ATTRIBUTE}": send(ATTRIBUTE)).count
-        return record_id + 1 unless record_id.zero?
+      def resource_id
+        id = self.class.where("#{ATTRIBUTE}": send(ATTRIBUTE)).count
+        return id unless id.zero?
       end
 
       # FIXME: title_changed? or attribute_changed? seems to be broken
       def should_generate_new_friendly_id?
-        # new_record? || attribute_changed?(ATTRIBUTE) || super
-        new_record? || super
+        new_record? || attribute_changed?(ATTRIBUTE) || super
       end
     end
   end
--- app/models/optional_modules/blogs/blog_category.rb
+++ app/models/optional_modules/blogs/blog_category.rb
@@ -16,34 +16,41 @@
 class BlogCategory < ApplicationRecord
   include Includes::BlogIncludable
 
-  translates :name, :slug, fallbacks_for_empty_translations: true
-  active_admin_translates :name, :slug, fallbacks_for_empty_translations: true do
-    validates :name,
+  ATTRIBUTE ||= :name
+  TRANSLATED_FIELDS ||= [:name, :slug].freeze
+
+  translates(*TRANSLATED_FIELDS, fallbacks_for_empty_translations: true)
+  active_admin_translates(*TRANSLATED_FIELDS, fallbacks_for_empty_translations: true) do
+    validates ATTRIBUTE,
               presence: true
   end
 
   extend FriendlyId
   friendly_id :slug_candidates,
               use: [:slugged,
-                    :history,
                     :globalize,
+                    # :history,
                     :finders]
 
-  # Models relations
+  # Model relations
   has_many :blogs, dependent: :destroy, inverse_of: :blog_category
 
   private
 
   def slug_candidates
-    [[:name, :deduced_id]]
+    [
+      ATTRIBUTE,
+      [ATTRIBUTE, resource_id]
+    ]
   end
 
-  def deduced_id
-    record_id = BlogCategory.where(name: name).count
-    return record_id + 1 unless record_id.zero?
+  def resource_id
+    id = self.class.where("#{ATTRIBUTE}": send(ATTRIBUTE)).count
+    return id unless id.zero?
   end
 
+  # FIXME: title_changed? or attribute_changed? seems to be broken
   def should_generate_new_friendly_id?
-    new_record? || attribute_changed?(:name)
+    new_record? || attribute_changed?(ATTRIBUTE) || super
   end
 end
