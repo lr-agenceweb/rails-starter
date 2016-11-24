5f5add21a10b53ddd8454a5ce74114c0d02c5876
Anthony Robin <contact@anthony-robin.fr>
2016-11-24 15:15:08 +0100

    AA Blog#form :: Add link to go to new blog category page in case of new category

--- test/decorators/optional_modules/blog_decorator_test.rb
+++ test/decorators/optional_modules/blog_decorator_test.rb
@@ -2,19 +2,24 @@
 require 'test_helper'
 
 #
-# == BlogDecorator test
-#
+# BlogDecorator test
+# ====================
 class BlogDecoratorTest < Draper::TestCase
   include Draper::LazyHelpers
 
   setup :initialize_test
 
   #
-  # == ActiveAdmin
-  #
+  # ActiveAdmin
+  # =============
   test 'should return correct AA show page title' do
     expected = I18n.t('post.title_aa_show', page: I18n.t('activerecord.models.blog.one'), title: @blog_decorated.title)
     assert_equal expected, @blog_decorated.title_aa_show
+  end
+
+  test 'should return correct html for create_blog_category_link' do
+    expected = 'Si votre catégorie n\'apparait pas dans la liste, vous pouvez <a class="button" href="/admin/blog_categories/new">l\'ajouter</a>'
+    assert_equal expected, @blog_decorated.create_blog_category_link
   end
 
   private
